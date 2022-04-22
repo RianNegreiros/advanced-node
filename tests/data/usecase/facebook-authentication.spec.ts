13fbf8daa1ef1c9bdd5dde14b39ba2672dea1de6
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-22 20:43:53 -0300

    ensure updateWithFacebook update account name

--- tests/data/usecase/facebook-authentication.spec.ts
+++ tests/data/usecase/facebook-authentication.spec.ts
@@ -19,6 +19,7 @@ describe('FacebookAuthenticationService', () => {
       email: 'any_fb_email'
     })
     userAccountRepo = mock()
+    userAccountRepo.load.mockResolvedValue(undefined)
     sut = new FacebookAuthenticationService(
       facebookApi,
       userAccountRepo)
@@ -47,8 +48,6 @@ describe('FacebookAuthenticationService', () => {
   })
 
   it('Should calls CreateUserAccountRepo when userAccountRepo returns undefined', async () => {
-    userAccountRepo.load.mockResolvedValueOnce(undefined)
-
     await sut.execute({ token })
 
     expect(userAccountRepo.createFromFacebook).toHaveBeenCalledWith({
@@ -70,6 +69,21 @@ describe('FacebookAuthenticationService', () => {
     expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledWith({
       id: 'any_id',
       name: 'any_name',
+      facebookId: 'any_fb_id'
+    })
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should update account name', async () => {
+    userAccountRepo.load.mockResolvedValueOnce({
+      id: 'any_id'
+    })
+
+    await sut.execute({ token })
+
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledWith({
+      id: 'any_id',
+      name: 'any_fb_name',
       facebookId: 'any_fb_id'
     })
     expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledTimes(1)
