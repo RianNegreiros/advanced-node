80b61bacac9a261bbae46bc12182e01c550f5c9a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-22 16:12:09 -0300

    ensure Facebook Authentication calls Facebook Api with correct params

---
+++ tests/data/usecase/facebook-authentication.spec.ts
@@ -0,0 +1,40 @@
+import { FacebookAuthentication } from '@/domain/usecases'
+
+class FacebookAuthenticationService {
+  constructor (
+    private readonly loadFacebookUserApi: LoadFacebookUserApi
+  ) {}
+
+  async execute (params: FacebookAuthentication.Params): Promise<void> {
+    await this.loadFacebookUserApi.loadUser(params)
+  }
+}
+
+interface LoadFacebookUserApi {
+  loadUser: (params: LoadFacebookUserApi.Params) => Promise<void>
+}
+
+namespace LoadFacebookUserApi {
+  export type Params = {
+    token: string
+  }
+}
+
+class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
+  token?: string
+
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
+    this.token = params.token
+  }
+}
+
+describe('FacebookAuthenticationService', () => {
+  it('Should call Facebook Api with correct params', async () => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+
+    await sut.execute({ token: 'any_token' })
+
+    expect(loadFacebookUserApi.token).toBe('any_token')
+  })
+})
