65be55c7f4a4f3c548403f366d2969c7a166d4a0
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-22 19:43:16 -0300

    add return type

--- src/domain/data/contracts/repos/user-account.ts
+++ src/domain/data/contracts/repos/user-account.ts
@@ -1,9 +1,11 @@
 export interface LoadUserAccountRepository {
-  load: (params: LoadUserAccountRepository.Params) => Promise<void>
+  load: (params: LoadUserAccountRepository.Params) => Promise<LoadUserAccountRepository.Result>
 }
 
 export namespace LoadUserAccountRepository {
   export type Params = {
     email: string
   }
+
+  export type Result = undefined
 }
