b4b8ead789065e95fc2507465148cdc2c905b908
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-27 07:33:38 -0300

    move RequiredStringValidator to its own file

--- tests/application/validation/required-string.spec.ts
+++ tests/application/validation/required-string.spec.ts
@@ -1,15 +1,5 @@
 import { RequiredFieldError } from '@/application/errors'
-
-class RequiredStringValidator {
-  constructor (
-    private readonly value: string,
-    private readonly fieldName: string
-  ) {}
-
-  validate (): Error | undefined {
-    return new RequiredFieldError('any_field')
-  }
-}
+import { RequiredStringValidator } from '@/application/validation'
 
 describe('RequiredStringValidator', () => {
   it('should return Error if value is empty', () => {
@@ -36,11 +26,11 @@ describe('RequiredStringValidator', () => {
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
 
-  it('should return Error if value is undefined', () => {
-    const sut = new RequiredStringValidator(undefined as any, 'any_field')
+  it('should return undefined if value is not empty', () => {
+    const sut = new RequiredStringValidator('any-value', 'any_field')
 
     const error = sut.validate()
 
-    expect(error).toEqual(new RequiredFieldError('any_field'))
+    expect(error).toBeUndefined()
   })
 })
