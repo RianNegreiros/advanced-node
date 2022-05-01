835dbab2baf55aeabbb84e899203d41fdddce22c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-30 22:07:47 -0300

    facebook login factories

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,6 @@ export const env = {
     clientId: process.env.FB_CLIENT_ID ?? '706295394152110',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '848b8f621f9a558089196fa023324079'
   },
-  port: process.env.PORT ?? 8080
+  port: process.env.PORT ?? 8080,
+  jwtSecret: process.env.JWT_SECRET ?? 'a0d0elj8kp'
 }
