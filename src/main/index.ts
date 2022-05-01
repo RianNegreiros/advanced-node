99ac8214f72b1238a8bcbc3cc9c3fc6561698aa8
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-04-30 23:54:07 -0300

    add port to env file

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,5 +2,6 @@ import './config/module-alias'
 import 'reflect-metadata'
 
 import { app } from '@/main/config/app'
+import { env } from './config/env'
 
-app.listen(8080, () => console.log('Server running at http://localhost:8080'))
+app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
