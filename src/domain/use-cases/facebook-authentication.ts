b5826dbbc8cd1a37a44ac2e88d1c6f2a41b129fc
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-04 11:02:45 -0300

    refactor

--- src/domain/use-cases/facebook-authentication.ts
+++ src/domain/use-cases/facebook-authentication.ts
@@ -15,12 +15,10 @@ export type FacebookAuthentication = (params: Input) => Promise<Output>
 
 export const setupFacebookAuthentication: Setup = (facebookApi, userAccountRepo, crypto) => async params => {
   const fbData = await facebookApi.loadUser(params)
-  if (fbData !== undefined) {
-    const accountData = await userAccountRepo.load({ email: fbData.email })
-    const fbAccount = new FacebookAccount(fbData, accountData)
-    const { id } = await userAccountRepo.saveWithFacebook(fbAccount)
-    const accessToken = await crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
-    return { accessToken }
-  }
-  throw new AuthenticationError()
+  if (fbData === undefined) throw new AuthenticationError()
+  const accountData = await userAccountRepo.load({ email: fbData.email })
+  const fbAccount = new FacebookAccount(fbData, accountData)
+  const { id } = await userAccountRepo.saveWithFacebook(fbAccount)
+  const accessToken = await crypto.generateToken({ key: id, expirationInMs: AccessToken.expirationInMs })
+  return { accessToken }
 }
