ea5994565b8e01b081bac8977c49b29f6c009cfe
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-06 13:28:38 -0300

    move producation code to their own file

---
+++ src/domain/use-cases/change-profile-picture.ts
@@ -0,0 +1,10 @@
+import { UploadFile } from '@/domain/contracts/gateways/'
+import { UUIDGenerator } from '@/domain/contracts/gateways'
+
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture
+type Input = { id: string, file: Buffer }
+export type ChangeProfilePicture = (input: Input) => Promise<void>
+
+export const setupChangeProfilePicture: Setup = (fileStorage, crypto) => async ({ id, file }) => {
+  await fileStorage.upload(({ file, key: crypto.uuid({ key: id }) }))
+}
