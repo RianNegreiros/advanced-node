9d1f6ed62e7e804a2491ff20212f3277e77436cf
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-06 13:33:08 -0300

    ensure ChangeProfilePicture not call UploadFile when file is undefined

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -2,9 +2,11 @@ import { UploadFile } from '@/domain/contracts/gateways/'
 import { UUIDGenerator } from '@/domain/contracts/gateways'
 
 type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture
-type Input = { id: string, file: Buffer }
+type Input = { id: string, file?: Buffer }
 export type ChangeProfilePicture = (input: Input) => Promise<void>
 
 export const setupChangeProfilePicture: Setup = (fileStorage, crypto) => async ({ id, file }) => {
-  await fileStorage.upload(({ file, key: crypto.uuid({ key: id }) }))
+  if (file !== undefined) {
+    await fileStorage.upload(({ file, key: crypto.uuid({ key: id }) }))
+  }
 }
