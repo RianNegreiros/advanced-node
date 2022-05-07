c593457eed915dd760d729d23c476de731fbac76
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-07 13:39:16 -0300

    ensure ChangeProfilePicture calls DeleteFile when file exists and SaveUserPicture throws

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -19,7 +19,8 @@ export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfil
   try {
     await userProfileRepo.savePicture(userProfile)
   } catch {
-    await fileStorage.delete({ key })
+    if (file !== undefined) await fileStorage.delete({ key })
+    throw new Error()
   }
   return userProfile
 }
