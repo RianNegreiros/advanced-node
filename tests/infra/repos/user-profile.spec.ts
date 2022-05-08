32b9e743c3c0eee3d2b5a2e4fbf9cb47d37c8978
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-08 15:38:54 -0300

    ensure LoadUserProfile load user profile

--- tests/infra/repos/user-profile.spec.ts
+++ tests/infra/repos/user-profile.spec.ts
@@ -35,4 +35,22 @@ describe('PgUserProfileRepository', () => {
       expect(PgUser).toMatchObject({ id, pictureUrl: 'any_url', initials: null })
     })
   })
+
+  describe('savePicture', () => {
+    it('should load user profile', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any_email', name: 'any_name', initials: 'any_initials' })
+
+      const userProfile = await sut.load({ id: id.toString() })
+
+      expect(userProfile?.name).toBe('any_name')
+    })
+
+    it('should load user profile', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any_email', name: 'any_name', initials: 'any_initials' })
+
+      const userProfile = await sut.load({ id: id.toString() })
+
+      expect(userProfile?.name).toBe('any_name')
+    })
+  })
 })
