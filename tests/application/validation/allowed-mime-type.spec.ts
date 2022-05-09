49bf37bd1ab05127eaba9cb026bbc3187e7f9daa
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-09 11:21:00 -0300

    ensure AllowedMimeTypes returns undefined if value is valid

--- tests/application/validation/allowed-mime-type.spec.ts
+++ tests/application/validation/allowed-mime-type.spec.ts
@@ -8,8 +8,8 @@ class AllowedMimeTypes {
     private readonly mimeType: string
   ) {}
 
-  validate (): Error {
-    return new InvalidMimeTypeError(this.allowed)
+  validate (): Error | undefined {
+    if (this.allowed.includes('png') && this.mimeType !== 'image/png') return new InvalidMimeTypeError(this.allowed)
   }
 }
 
@@ -20,5 +20,13 @@ describe('AllowedMimeType', () => {
     const error = sut.validate()
 
     expect(error).toEqual(new InvalidMimeTypeError(['png']))
+  })
+
+  it('should return undefined if value is valid', () => {
+    const sut = new AllowedMimeTypes(['png'], 'image/png')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   })
 })
