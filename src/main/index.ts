ed7eda3591840844651596a2ba648866df945d4a
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-10 13:04:38 -0300

    typeorm connection

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,14 @@
 import './config/module-alias'
 import 'reflect-metadata'
 
-import { app } from '@/main/config/app'
 import { env } from './config/env'
+import { createConnection, getConnectionOptions } from 'typeorm'
 
-app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+getConnectionOptions()
+  .then(async options => {
+    const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'
+    const entities = [`${root}/infra/repos/postgres/entities/index.{js,ts}`]
+    await createConnection({ ...options, entities })
+    const { app } = await import('@/main/config/app')
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  }).catch(console.error)
