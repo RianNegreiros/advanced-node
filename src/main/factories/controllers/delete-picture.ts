a76165e5ef80dda6b8648c5ccc6796d0793e2686
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-10 11:32:05 -0300

    merge delete and save picture controllers

--- src/main/factories/controllers/delete-picture.ts
+++
@@ -1,6 +0,0 @@
-import { DeletePictureController } from '@/application/controllers'
-import { makeChangeProfilePicture } from '../use-cases'
-
-export const makeDeletePictureController = (): DeletePictureController => {
-  return new DeletePictureController(makeChangeProfilePicture())
-}
