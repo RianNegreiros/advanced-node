8e26c0cc9b51763a2d66eb0d0bd23c48de3d442f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-05-10 12:59:12 -0300

    ensure PUT /users/picture return 200 with valid data

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,10 +1,10 @@
 import { auth } from '@/main/middlewares'
-import { adaptExpressRoute } from '@/main/adapters'
+import { adaptExpressRoute, adaptMulter } from '@/main/adapters'
 import { makeSavePictureController } from '@/main/factories/controllers'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.delete('/users/picture', auth, adaptExpressRoute(makeSavePictureController()))
-  router.put('/users/picture', auth)
+  router.put('/users/picture', auth, adaptMulter, adaptExpressRoute(makeSavePictureController()))
 }
